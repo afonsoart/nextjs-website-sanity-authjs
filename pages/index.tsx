aee7762aee3a1e33e4f42c0f4f06b4b60e14ba57
Afonso Ramon de Araujo Teles <afonso1907@hotmail.com>
2023-11-06 16:11:06 -0300

    Finishing Posts Internal page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ interface Props {
   posts: [Post];
 }
 
-export default function Home({posts}: Props) {
+export default function Home({ posts }: Props) {
 
   return (
     <div>
@@ -34,26 +34,42 @@ export default function Home({posts}: Props) {
           <BannerBottom />
         </div>
         {/* ============ Banner-Bottom End here ======= */}
-        
+
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3 md:gap-6 py-6">
-        {
-            posts.map((post, index)=> (
+          {
+            posts.map((post) => (
               <Link key={post._id} href={`/post/${post.slug.current}`}>
-              <div className="border-[1px] border-secondaryColor border-opacity-40 h-[450px] group">
-              <div className=" h-3/5 w-full overflow-hidden">
-                <Image 
-                src={urlFor(post.mainImage).url()!} 
-                alt="Main Image" 
-                width={380} 
-                height={350}
-                className=" w-full h-full object-cover brightness-75 group-hover:brightness-100 duration-300 group-hover:scale-110"/>
-              </div>
-              </div>
+                <div className="border-[1px] border-secondaryColor border-opacity-40 h-[450px] group">
+                  <div className=" h-3/5 w-full overflow-hidden">
+                    <Image
+                      src={urlFor(post.mainImage).url()!}
+                      alt="Main Image"
+                      width={380}
+                      height={350}
+                      className=" w-full h-full object-cover brightness-75 group-hover:brightness-100 duration-300 group-hover:scale-110" />
+                  </div>
+                  <div className=" h-2/5 w-full flex flex-col justify-center">
+                    <div className="flex justify-between items-center px-4 py-1 border-b-[1px] border-b-gray-500">
+                      <p>{post.title}</p>
+                      <Image
+                      className="w-12 h-12 rounded-full origin-center"
+                      src={urlFor(post.author.image).url()!} 
+                      alt="authorImg" 
+                      width={48}
+                      height={48}
+                      />
+                    </div>
+                    <p className="py-2 px-4 text-base">
+                      {post.description.substring(0, 60)}... by -{" "} 
+                      <span className="font-semibold"> {post.author.name} </span>
+                    </p>
+                  </div>
+                </div>
               </Link>
             ))
           }
         </div>
-        
+
         {/* ============ Post Part End here =========== */}
         {/* ============ Footer Start here============= */}
         <Footer />
@@ -77,7 +93,7 @@ export const getServerSideProps = async () => {
   }`
   const posts = await sanityClient.fetch(query);
   return {
-    props:{
+    props: {
       posts,
     },
   };
